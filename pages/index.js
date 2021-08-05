55e360a6d713ff8960fa93604f626770c8f93b48
Omar <omaar_5@hotmail.com>
2021-08-05 21:10:17 +0200

    add: HeadTag

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,13 @@
+import Head from "next/head";
+
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>Omar's Portfolio | Digital Resume</title>
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+      </Head>
+
       <h1>Working on my portfolio with NextJS !!!</h1>
       <h2>Working on my portfolio with NextJS !!!</h2>
       <h3>Working on my portfolio with NextJS !!!</h3>
