2707fafea20d66ea5f92d8426e3885bbee220641
Omar <omaar_5@hotmail.com>
2021-08-05 20:45:29 +0200

    fix: cleanup the build

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Working on my portfolio with NextJS !!!</h1>
+      <h2>Working on my portfolio with NextJS !!!</h2>
+      <h3>Working on my portfolio with NextJS !!!</h3>
+      <h4>Working on my portfolio with NextJS !!!</h4>
+      <h5>Working on my portfolio with NextJS !!!</h5>
+      <h6>Working on my portfolio with NextJS !!!</h6>
+    </div>
+  );
+};
