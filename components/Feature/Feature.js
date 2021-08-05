8d707e45a1a38f75f68f8f33deb19af6d4ad8c86
Omar <omaar_5@hotmail.com>
2021-08-05 21:19:50 +0200

    setup: Feature

---
+++ components/Feature/Feature.js
@@ -0,0 +1,11 @@
+import {
+    Container,
+} from "./styledFeature";
+
+export const Feature = () => {
+    return (
+        <Container>
+            Feature component
+        </Container>
+    );
+};
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,35 @@
 import Head from "next/head";
+import styled from 'styled-components';
+import { Feature } from "../components";
 
 export default function Home() {
   return (
-    <div>
+    <Application>
       <Head>
         <title>Omar's Portfolio | Digital Resume</title>
         <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
       </Head>
 
-      <h1>Working on my portfolio with NextJS !!!</h1>
-      <h2>Working on my portfolio with NextJS !!!</h2>
-      <h3>Working on my portfolio with NextJS !!!</h3>
-      <h4>Working on my portfolio with NextJS !!!</h4>
-      <h5>Working on my portfolio with NextJS !!!</h5>
-      <h6>Working on my portfolio with NextJS !!!</h6>
-    </div>
+      <Main>
+        <Feature />
+      </Main>
+    </Application>
   );
 };
+
+const Application = styled.div``;
+
+const Main = styled.main`
+  padding: 2rem 0 4rem;
+  max-width: 768px;
+  margin: 0 1.5rem;
+  
+  @media screen and (min-width: 768px) {
+    padding: 6rem 0 2rem;
+    margin: 0 auto;
+  }
+
+  @media screen and (max-width: 350px) {
+    margin: 0 1rem;
+  }
+`;
