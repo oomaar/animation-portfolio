38a297810aeaa14d6384a15f6dcff1d06af949ac
Omar <omaar_5@hotmail.com>
2021-08-05 20:53:32 +0200

    style: add font

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,11 @@
-// import { createGlobalStyle, ThemeProvider } from 'styled-components'
-
-// const GlobalStyle = createGlobalStyle`
-//   body {
-//     margin: 0;
-//     padding: 0;
-//     box-sizing: border-box;
-//   }
-// `;
-
-// const theme = {
-//   colors: {},
-// };
+// import { ThemeProvider } from 'styled-components'
+import "../font.css";
+import { GlobalStyle } from "../GlobalStyle";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      {/* <GlobalStyle /> */}
+      <GlobalStyle />
       {/* <ThemeProvider theme={theme}> */}
       <Component {...pageProps} />
       {/* </ThemeProvider> */}
