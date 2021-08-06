ab5535d584166ef1bb6a500703d7bb4ee0fb9f4a
Omar <omaar_5@hotmail.com>
2021-08-06 15:53:18 +0200

    complete: About

--- components/About/About.js
+++ components/About/About.js
@@ -1,13 +1,24 @@
-import { Section } from "../../GlobalStyle";
+import { Section, SectionTitle } from "../../GlobalStyle";
 import {
     Container,
+    SubContainer,
+    AboutMe,
+    Button,
 } from "./styledAbout";
 
-export const About = () => {
+export const About = ({ data }) => {
     return (
         <Section>
             <Container>
-                About
+                <SubContainer>
+                    <SectionTitle>About</SectionTitle>
+                </SubContainer>
+                <AboutMe>{data.description}</AboutMe>
+                <SubContainer>
+                    <Button download="" href="#">
+                        Download My Resume
+                    </Button>
+                </SubContainer>
             </Container>
         </Section>
     );
--- components/About/styledAbout.js
+++ components/About/styledAbout.js
@@ -1,3 +1,71 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+import { lgScreen, mdScreen, xlScreen } from '../../GlobalStyle';
 
-export const Container = styled.div``;
+// const opacityAnimation = 
+//   from {
+//     opacity: 0;
+//   } to {
+//     opacity: 1;
+//   }
+// `;
+
+const translateLeft = keyframes`
+  from {
+    transform: translateX(2000px);
+    opacity: 0;
+  } to {
+    transform: translateX(0);
+    opacity: 1;
+  }
+`;
+
+const translateRight = keyframes`
+  from {
+    transform: translateX(-2000px);
+    opacity: 0;
+   } to {
+    transform: translateX(0);
+    opacity: 1;
+   }
+`;
+
+export const Container = styled.div`
+  @media screen and (min-width: ${lgScreen}px) {
+    padding-left: 1rem;
+  }
+`;
+
+export const SubContainer = styled.div`
+  animation: ${translateRight} 2s ease none;
+`;
+
+export const AboutMe = styled.p`
+  margin-bottom: 3rem;
+  font-size: 1rem;
+  animation: ${translateLeft} 2s ease none;
+  width: 100%;
+`;
+
+export const Button = styled.a`
+  padding: 0.8rem 1.6rem;
+  background-color: ${({ theme }) => theme.colors.firstColor};
+  color: #fff;
+  border-radius: 1rem;
+  text-transform: uppercase;
+  letter-spacing: 0.1rem;
+  font-size: 0.8rem;
+  transition: 0.5s;
+  opacity: 0.94;
+
+  :hover {
+    opacity: 1;
+  }
+
+  @media screen and (min-width: ${mdScreen}px) {
+    font-size: 0.9rem;
+  }
+
+  @media screen and (min-width: ${xlScreen}px) {
+    font-size: 1rem;
+  }
+`;
