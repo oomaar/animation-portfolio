19d93c07a3f5ac9dede63d239d3a325e4a5dd6c0
Omar <omaar_5@hotmail.com>
2021-08-06 02:13:22 +0200

    complete: Feature

--- components/Feature/Feature.js
+++ components/Feature/Feature.js
@@ -1,11 +1,56 @@
+import Typewriter from 'typewriter-effect';
+import { FaLinkedinIn } from "react-icons/fa";
+import { FiGithub } from "react-icons/fi";
+import { FaFacebook } from "react-icons/fa";
+import { Section } from '../../GlobalStyle';
 import {
     Container,
+    SubContainer,
+    Greating,
+    Title,
+    Writer,
+    Bio,
+    Button,
+    ImageContainer,
+    Social,
+    SocialIcon,
 } from "./styledFeature";
 
-export const Feature = () => {
+export const Feature = ({ data }) => {
     return (
-        <Container>
-            Feature component
-        </Container>
+        <Section>
+            <Container>
+                <SubContainer>
+                    <Greating>Hi there,</Greating>
+                    <Title>I am <span>{data.name}</span></Title>
+                    <Writer>
+                        <Typewriter
+                            options={{
+                                strings: ['Frontend Developer', 'Web Designer', 'Web Developer'],
+                                autoStart: true,
+                                loop: true,
+                            }}
+                        />
+                    </Writer>
+                    <Bio>{data.bio}</Bio>
+                    <Button href="#">Hire me</Button>
+
+                    <Social>
+                        <SocialIcon href="https://github.com/oomaar">
+                            <FiGithub />
+                        </SocialIcon>
+                        <SocialIcon href="https://www.linkedin.com/in/omar-hassan-8b1869137">
+                            <FaLinkedinIn />
+                        </SocialIcon>
+                        <SocialIcon href="https://www.facebook.com/omarhassan16694">
+                            <FaFacebook />
+                        </SocialIcon>
+                    </Social>
+                </SubContainer>
+                <ImageContainer>
+                    <img src="/feature.png" />
+                </ImageContainer>
+            </Container>
+        </Section>
     );
 };
--- components/Feature/styledFeature.js
+++ components/Feature/styledFeature.js
@@ -1,3 +1,171 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+import { lgScreen, mdScreen, xlScreen } from '../../GlobalStyle';
 
-export const Container = styled.div``;
+const imageTranslate = keyframes`
+  from {
+    transform: translateX(1000px);
+    opacity: 0;
+  } to {
+    transform: translateX(0);
+    opacity: 1;
+  }
+`;
+
+const featureTranslate = keyframes`
+  from {
+    transform: translateX(-1000px);
+    opacity: 0;
+  } to {
+    transform: translateX(0);
+    opacity: 1;
+  }
+`;
+
+const titleTranslate = keyframes`
+  from {
+    opacity: 0;
+    transform: translateY(50px);
+  } to {
+    transform: translateY(0);
+    opacity: 1;
+  }
+`;
+
+const opacityAnimation = keyframes`
+  from {
+    opacity: 0;
+  } to {
+    opacity: 1;
+  }
+`;
+
+export const Container = styled.div`
+  display: flex;
+`;
+
+export const SubContainer = styled.div`
+  width: 100%;
+  animation: ${featureTranslate} 2s ease none;
+
+  @media screen and (min-width: ${lgScreen}px) {
+    width: 50%;
+    padding-left: 1rem;
+  }
+`;
+
+export const Greating = styled.h4`
+  font-family: 'Indie Flower', cursive;
+  font-size: 1.3rem;
+  animation: ${opacityAnimation} 2s ease none;
+  
+  @media screen and (min-width: ${mdScreen}px) {
+    font-size: 1.5rem;
+  }
+`;
+
+export const Title = styled.h1`
+  font-size: 2rem;
+  animation: ${titleTranslate} 2s ease none;
+
+  span {
+    color: ${({ theme }) => theme.colors.firstColor};
+  }
+
+  @media screen and (min-width: ${mdScreen}px) {
+    font-size: 2.5rem;
+  }
+
+  @media screen and (min-width: ${xlScreen}px) {
+    font-size: 3rem;
+  }
+`;
+
+export const Writer = styled.div`
+  font-family: 'Source Code Pro', monospace;
+  font-size: 1.3rem;
+
+  @media screen and (min-width: ${mdScreen}px) {
+    font-size: 1.5rem;
+  }
+
+  @media screen and (min-width: ${xlScreen}px) {
+    font-size: 2rem;
+  }
+`;
+
+export const Bio = styled.p`
+  font-size: 1rem;
+  margin: 2rem 0;
+  animation: ${opacityAnimation} 2s ease none;
+  width: 270px;
+
+  @media screen and (min-width: ${mdScreen}px) {
+    width: 300px;
+  }
+
+  @media screen and (min-width: ${xlScreen}px) {
+    width: 350px;
+  }
+`;
+
+export const Button = styled.a`
+  padding: 0.8rem 1.6rem;
+  background-color: ${({ theme }) => theme.colors.firstColor};
+  color: #fff;
+  border-radius: 1rem;
+  text-transform: uppercase;
+  letter-spacing: 0.1rem;
+  font-size: 0.8rem;
+  animation: ${opacityAnimation} 2s ease none 0;
+  transition: 0.5s;
+  opacity: 0.94;
+
+  :hover {
+    opacity: 1;
+  }
+
+  @media screen and (min-width: ${mdScreen}px) {
+    font-size: 0.9rem;
+  }
+
+  @media screen and (min-width: ${xlScreen}px) {
+    font-size: 1rem;
+  }
+`;
+
+export const Social = styled.div`
+  display: flex;
+  margin-top: 2.5rem;
+  align-items: center;
+  animation: ${opacityAnimation} 2s ease none 0s;
+`;
+
+export const SocialIcon = styled.a`
+  color: ${({ theme }) => theme.colors.firstColor};
+  cursor: pointer;
+  margin: 0 0.5rem;
+  font-size: 1.3rem;
+  transition: 0.5s;
+
+  :hover {
+    transform: scale(1.2);
+  }
+
+  @media screen and (min-width: ${mdScreen}px) {
+    font-size: 1.5rem;
+  }
+
+  @media screen and (min-width: ${xlScreen}px) {
+    font-size: 1.7rem;
+  }
+`;
+
+export const ImageContainer = styled.div`
+  display: none;
+
+  @media screen and (min-width: ${lgScreen}px) {
+    display: block;
+    width: 50%;
+    animation: ${imageTranslate} 2s ease none 0s;
+  }
+`;
