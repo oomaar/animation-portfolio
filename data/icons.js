f0c01ecfb7996891c12e638c9a378267dea8c619
Omar <omaar_5@hotmail.com>
2021-08-11 23:21:09 +0200

    fix: Skills

--- data/icons.js
+++ data/icons.js
@@ -4,40 +4,69 @@ import { FaReact, FaNodeJs } from "react-icons/fa";
 import { DiMongodb } from "react-icons/di";
 import { BiGitBranch } from "react-icons/bi";
 import { FiGithub } from "react-icons/fi";
-import { Icon, IconsContainer } from "../components/Skills/styledSkills";
+import { Icon, IconsContainer, SkillTitle, SubContainer } from "../components/Skills/styledSkills";
 
 export const iconsData = () => (
     <IconsContainer>
-        <Icon>
-            <RiHtml5Fill />
-        </Icon>
-        <Icon>
-            <SiCss3 />
-        </Icon>
-        <Icon>
-            <SiJavascript />
-        </Icon>
-        <Icon>
-            <FaReact />
-        </Icon>
-        <Icon>
-            <SiRedux />
-        </Icon>
-        <Icon>
-            <FiGithub />
-        </Icon>
-        <Icon>
-            <BiGitBranch />
-        </Icon>
-        <Icon>
-            <SiFirebase />
-
-        </Icon>
-        <Icon>
-            <FaNodeJs />
-        </Icon>
-        <Icon>
-            <DiMongodb />
-        </Icon>
+        <SubContainer>
+            <Icon>
+                <RiHtml5Fill />
+            </Icon>
+            <SkillTitle>HTML-5</SkillTitle>
+        </SubContainer>
+        <SubContainer>
+            <Icon>
+                <SiCss3 />
+            </Icon>
+            <SkillTitle>CSS-3</SkillTitle>
+        </SubContainer>
+        <SubContainer>
+            <Icon>
+                <SiJavascript />
+            </Icon>
+            <SkillTitle>Javascript-ES6</SkillTitle>
+        </SubContainer>
+        <SubContainer>
+            <Icon>
+                <FaReact />
+            </Icon>
+            <SkillTitle>ReactJS</SkillTitle>
+        </SubContainer>
+        <SubContainer>
+            <Icon>
+                <SiRedux />
+            </Icon>
+            <SkillTitle>Redux</SkillTitle>
+        </SubContainer>
+        <SubContainer>
+            <Icon>
+                <FiGithub />
+            </Icon>
+            <SkillTitle>Gitub</SkillTitle>
+        </SubContainer>
+        <SubContainer>
+            <Icon>
+                <BiGitBranch />
+            </Icon>
+            <SkillTitle>Git</SkillTitle>
+        </SubContainer>
+        <SubContainer>
+            <Icon>
+                <SiFirebase />
+            </Icon>
+            <SkillTitle>Firebase</SkillTitle>
+        </SubContainer>
+        <SubContainer>
+            <Icon>
+                <FaNodeJs />
+            </Icon>
+            <SkillTitle>NodeJS</SkillTitle>
+        </SubContainer>
+        <SubContainer>
+            <Icon>
+                <DiMongodb />
+            </Icon>
+            <SkillTitle>MongoDB</SkillTitle>
+        </SubContainer>
     </IconsContainer>
 );
