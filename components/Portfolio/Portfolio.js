395ff010b5c1bd90def0650e39c9a5ae0591954b
Omar <omaar_5@hotmail.com>
2021-08-11 21:16:35 +0200

    complete: Portfolio

--- components/Portfolio/Portfolio.js
+++ components/Portfolio/Portfolio.js
@@ -1,14 +1,153 @@
+import { useState, useRef } from "react";
+import { Button, Section, SectionTitle } from "../../Global/GlobalStyle";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Navigation, Pagination, Scrollbar } from 'swiper';
+import 'swiper/swiper-bundle.css';
 import {
     Container,
+    LinksContainer,
+    Link,
+    CarouselContainer,
+    PortfolioContainer,
+    ImageContainer,
+    PortfolioImage,
+    TextContainer,
+    PortfolioTitle,
+    PortfolioText,
+    ArrowNext,
+    SwiperPortfolioIcon,
+    ArrowPrev,
+    ProjectContainer,
 } from "./styledPortfolio";
 
+SwiperCore.use([Navigation, Pagination, Scrollbar]);
+
 export const Portfolio = ({ data }) => {
+    const [category, setCategory] = useState("all");
+    const navigationPrevRef = useRef(null);
+    const navigationNextRef = useRef(null);
+
+    const cloneData = data.clone.map((clone, i) => (
+        <ProjectContainer>
+            <SwiperSlide key={i}>
+                <PortfolioContainer>
+                    <ImageContainer href={clone.url}>
+                        <PortfolioImage src={clone.image} alt="" />
+                    </ImageContainer>
+                    <TextContainer>
+                        <PortfolioTitle>{clone.title}</PortfolioTitle>
+                        <PortfolioText>{clone.description}</PortfolioText>
+                        <Button link href={clone.url}>Demo</Button>
+                    </TextContainer>
+                </PortfolioContainer>
+            </SwiperSlide>
+        </ProjectContainer>
+    ));
+
+    const schoolData = data.school.map((school, i) => (
+        <ProjectContainer>
+            <SwiperSlide key={i}>
+                <PortfolioContainer>
+                    <ImageContainer href={school.url}>
+                        <PortfolioImage src={school.image} alt="" />
+                    </ImageContainer>
+                    <TextContainer>
+                        <PortfolioTitle>{school.title}</PortfolioTitle>
+                        <PortfolioText>{school.description}</PortfolioText>
+                        <Button link href={school.url}>Demo</Button>
+                    </TextContainer>
+                </PortfolioContainer>
+            </SwiperSlide>
+        </ProjectContainer>
+    ));
+
+    const freeData = data.free.map((free, i) => (
+        <ProjectContainer>
+            <SwiperSlide key={i}>
+                <PortfolioContainer>
+                    <ImageContainer href={free.url}>
+                        <PortfolioImage src={free.image} alt="" />
+                    </ImageContainer>
+                    <TextContainer>
+                        <PortfolioTitle>{free.title}</PortfolioTitle>
+                        <PortfolioText>{free.description}</PortfolioText>
+                        <Button link href={free.url}>Demo</Button>
+                    </TextContainer>
+                </PortfolioContainer>
+            </SwiperSlide>
+        </ProjectContainer>
+    ));
+
     return (
-        <Container>
-            <h1>Portfolio Component</h1>
-            {/* Categories style */}
-            {/* Navbar: All, Clones, School, Freelance */}
-            {/* Carousel: react-responsive-carousel or swiper ! */}
-        </Container>
+        <Section data-aos="zoom-in-down">
+            <Container>
+                <SectionTitle>Portfolio</SectionTitle>
+
+                <LinksContainer>
+                    <Link onClick={() => setCategory("all")}>All</Link>
+                    <Link onClick={() => setCategory("school")}>School</Link>
+                    <Link onClick={() => setCategory("clone")}>Clones</Link>
+                    <Link onClick={() => setCategory("free")}>Freelance</Link>
+                </LinksContainer>
+
+                <CarouselContainer>
+                    <Swiper
+                        loop='true'
+                        navigation={{
+                            nextEl: navigationNextRef.current,
+                            prevEl: navigationPrevRef.current
+                        }}
+                        onSwiper={(swiper) => {
+                            // Delay execution for the refs to be defined
+                            setTimeout(() => {
+                                // Override prevEl & nextEl now that refs are defined
+                                swiper.params.navigation.prevEl = navigationPrevRef.current
+                                swiper.params.navigation.nextEl = navigationNextRef.current
+
+                                // Re-init navigation
+                                swiper.navigation.destroy()
+                                swiper.navigation.init()
+                                swiper.navigation.update()
+                            })
+                        }}
+                        pagination={{ clickable: true }}
+                    >
+                        {category === "all" && (
+                            <div>
+                                {cloneData}
+                                {schoolData}
+                                {freeData}
+                            </div>
+                        )}
+                        {category === "school" && (
+                            <div>
+                                {schoolData}
+                            </div>
+                        )}
+                        {category === "clone" && (
+                            <div>
+                                {cloneData}
+                            </div>
+                        )}
+                        {category === "free" && (
+                            <div>
+                                {freeData}
+                            </div>
+                        )}
+                    </Swiper>
+                    <ArrowNext ref={navigationNextRef} className="swiper-button-next">
+                        <SwiperPortfolioIcon>
+                            &#10095;
+                        </SwiperPortfolioIcon>
+                    </ArrowNext>
+                    <ArrowPrev ref={navigationPrevRef} className="swiper-button-prev">
+                        <SwiperPortfolioIcon>
+                            &#10094;
+                        </SwiperPortfolioIcon>
+                    </ArrowPrev>
+                    <div className="swiper-pagination"></div>
+                </CarouselContainer>
+            </Container>
+        </Section>
     );
 };
--- components/Portfolio/styledPortfolio.js
+++ components/Portfolio/styledPortfolio.js
@@ -1,3 +1,212 @@
 import styled from 'styled-components';
+import { zoom } from '../../Global/GlobalAnimation';
+import { lgScreen, mdScreen, xlScreen } from '../../Global/GlobalStyle';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  @media screen and (min-width: ${lgScreen}px) {
+    padding-left: 1rem;
+  }
+`;
+
+export const LinksContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  @media screen and (min-width: ${mdScreen}px) {
+    justify-content: space-evenly;
+  }
+`;
+
+export const Link = styled.span`
+  cursor: pointer;
+  font-size: 1rem;
+  transition: 0.5s;
+
+  :hover {
+    transform: scale(1.1);
+  }
+
+  @media screen and (min-width: ${xlScreen}px) {
+    font-size: 1.2rem;
+  }
+`;
+
+export const CarouselContainer = styled.div`
+  padding: 0.5rem;
+  margin: 2rem 0 0;
+  cursor: grab;
+
+  .swiper-container-horizontal > .swiper-pagination-bullets {
+    bottom: 0;
+  }
+  .swiper-pagination-bullet-active {
+    background-color: ${({ theme }) => theme.colors.firstColor};
+  }
+  .swiper-button-prev,
+  .swiper-button-next,
+  .swiper-pagination-bullet {
+    outline: none;
+  }
+
+  @media screen and (min-width: ${mdScreen}px) {
+    /* padding: 1rem; */
+  }
+
+  @media screen and (min-width: ${lgScreen}px) {
+  }
+`;
+
+export const PortfolioContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 2rem;
+
+  @media screen and (min-width: ${mdScreen}px) {
+    padding: 2rem 3rem;
+  }
+
+  @media screen and (min-width: ${lgScreen}px) {
+    flex-direction: row;
+  }
+`;
+
+export const ImageContainer = styled.a`
+  box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
+  border-radius: 5%;
+  background: transparent;
+
+  @media screen and (min-width: ${lgScreen}px) {
+    margin-right: 1rem;
+  }
+`;
+
+export const PortfolioImage = styled.img`
+  width: 220px;
+  border-radius: 5%;
+  cursor: pointer;
+  transition: 0.5s;
+
+  :hover {
+    transform: scale(1.1);
+  }
+
+  @media screen and (min-width: ${mdScreen}px) {
+    width: 500px;
+  }
+`;
+
+export const TextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 0 1rem;
+  justify-content: center;
+`;
+
+export const PortfolioTitle = styled.h3`
+  text-align: left;
+  font-size: 1.1rem;
+
+  @media screen and (min-width: ${mdScreen}px) {
+    font-size: 1.5rem;
+  }
+`;
+
+export const PortfolioText = styled.span`
+  text-align: left;
+  margin: 1rem 0;
+  font-size: 0.9rem;
+
+  @media screen and (min-width: ${mdScreen}px) {
+    font-size: 1rem;
+  }
+`;
+
+export const ArrowNext = styled.div`
+  right: -1rem;
+  top: 60%;
+  
+  ::after {
+    content: '';
+  }
+  
+  @media screen and (min-width: ${lgScreen}px) {
+    right: 1rem;
+  }
+
+  @media screen and (min-width: ${xlScreen}px) {
+    right: 0.5rem;
+  }
+`;
+
+export const SwiperPortfolioIcon = styled.div`
+  font-size: 2rem;
+  color: ${({ theme }) => theme.colors.firstColor};
+  cursor: pointer;
+  
+  @media screen and (min-width: ${xlScreen}px) {
+    font-size: 3.5rem;
+  }
+`;
+
+export const ArrowPrev = styled.div`
+  left: -1rem;
+  top: 60%;
+
+  ::after {
+    content: '';
+  }
+
+  @media screen and (min-width: ${lgScreen}px) {
+    left: 1rem;
+  }
+
+  @media screen and (min-width: ${xlScreen}px) {
+    left: 0.5rem;
+  }
+`;
+
+export const ProjectContainer = styled.div`
+  animation: ${zoom} 0.1s linear none;
+`;
+
+
+
+/* .carousel.carousel-slider {
+  overflow: inherit;
+}
+
+.carousel .control-next.control-arrow, .carousel .control-next.control-arrow:hover{
+  background-color: transparent;
+  right: -10px;
+}
+
+.carousel .control-prev.control-arrow, .carousel .control-prev.control-arrow:hover {
+  background-color: transparent;
+  left: -10px;
+}
+
+.carousel .control-arrow, .carousel.carousel-slider .control-arrow{
+  opacity: 1;
+}
+
+.carousel .control-next.control-arrow:before {
+  content: '';
+  border: solid #0135AD;
+  border-width: 0 8px 8px 0;
+  display: inline-block;
+  padding: 10px;
+  transform: rotate(-45deg);
+  -webkit-transform: rotate(-45deg);
+}
+
+.carousel .control-prev.control-arrow:before {
+  content: '';
+  border: solid #0135AD;
+  border-width: 0 8px 8px 0;
+  display: inline-block;
+  padding: 10px;
+  transform: rotate(135deg);
+  -webkit-transform: rotate(135deg);
+} */
